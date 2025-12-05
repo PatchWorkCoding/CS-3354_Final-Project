59ab765043adf767e39430ebaddec8aadb16014d
patchworkcoding <Rose@NEW-MACHINE-01.com>
2025-12-04 18:09:20 -0600

    Finished Basic functionality of Creating, Open, and Saving new notes

--- app/src/main/java/cs/_final/project/Note.java
+++ app/src/main/java/cs/_final/project/Note.java
@@ -1,5 +1,63 @@
 package cs._final.project;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Scanner;
 
-public class Note {
+import javax.swing.*;
+
+public class Note extends WindowAdapter {
+    File saveFile;
+    JTextArea inputField;
+    String text = "";
     
+    public Note(File _data) {
+        saveFile = _data;
+        Scanner Reader;
+        try {
+            Reader = new Scanner(saveFile);    
+        } catch (Exception e) {
+            System.err.println(e.toString());
+            return;
+        }
+
+        while (Reader.hasNextLine()) {
+            System.out.println(text);
+            text += Reader.nextLine();
+            text += System.lineSeparator();
+            
+        }
+
+        //Create Window
+        JFrame frame = new JFrame(_data.getName());
+        frame.setSize(800, 600);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.addWindowListener(this);
+        //frame.addWindowStateListener(null);
+        frame.setVisible(true);
+
+        inputField = new JTextArea();
+        inputField.append(text);
+        frame.add(inputField);
+        
+        Reader.close();
+    }
+
+    @Override
+    public void windowClosing(WindowEvent e) {
+        
+        
+        try {
+            text = inputField.getText();
+            FileWriter writer = new FileWriter(saveFile.getAbsolutePath());
+            writer.write(text);
+            writer.close();
+        } catch (Exception exception) {
+            System.err.println(exception.toString());
+        }
+        
+
+        super.windowClosing(e);
+    }
 }
--- app/src/main/java/cs/_final/project/NoteManager.java
+++ app/src/main/java/cs/_final/project/NoteManager.java
@@ -3,48 +3,88 @@ package cs._final.project;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.File;
-
 import javax.swing.*;
-import javax.swing.border.Border;
+
 public class NoteManager implements ActionListener {
-    int counter = 0;
+    int numberOfNotes = 0;
+    String saveDirectory = "Note_Folder";
+    JPanel notesField;
+    JPanel NotesPanel;
+
     
+
     public NoteManager() {
+        CreateWindow();
+        PopulateNotesField();
+    }
+
+    void CreateWindow() {
         //Create Window
         JFrame frame = new JFrame("FILE CABINET");
         frame.setSize(800, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
-        
-        JPanel contentPanel = new JPanel(new BorderLayout());
-        frame.add(contentPanel);
 
-        File NotesFolder = new File("Note_Folder");
-        File[] noteFiles = NotesFolder.listFiles();
+        notesField = new JPanel(new BorderLayout());
+        frame.add(notesField);
 
-        if (noteFiles != null && noteFiles.length > 0) {
-            JPanel NotesPanel = new JPanel(new GridLayout(0,2, 10, 10));
-
-            for (File file : noteFiles) {
-                System.out.println("Listing Files");
-                JButton OpenNoteButton = new JButton("open: " + file.getName());
-                NotesPanel.add(OpenNoteButton);          
-            }
-            contentPanel.add(NotesPanel, BorderLayout.CENTER);
-        }
-        else {
-            contentPanel.add(new JLabel("No Notes Exist", 0), BorderLayout.CENTER);
-        }
 
         JButton CreateNewNoteButton = new JButton("Create Note");
+        CreateNewNoteButton.setActionCommand("Create");
         CreateNewNoteButton.addActionListener(this);
-        contentPanel.add(CreateNewNoteButton, BorderLayout.PAGE_END);
+        notesField.add(CreateNewNoteButton, BorderLayout.PAGE_END);
+    }
+
+    void PopulateNotesField() {
+        NotesPanel = new JPanel(new GridLayout(0,2, 10, 10));
+        notesField.add(NotesPanel, BorderLayout.CENTER);
+
+        File NotesFolder = new File(saveDirectory);
+        if(!NotesFolder.exists())
+            NotesFolder.mkdir();
+
+        File[] noteFiles = NotesFolder.listFiles();
+        
+        
+        if (noteFiles != null && noteFiles.length > 0) {
+            numberOfNotes = noteFiles.length;
+
+            for (File file : noteFiles) {
+                AddNoteEntryButton(file);
+            }
+
+        }
+    }
+
+    void AddNoteEntryButton(File file) {
+            JButton OpenNoteButton = new JButton("open: " + file.getName());
+
+            OpenNoteButton.setActionCommand(file.getName());
+            OpenNoteButton.addActionListener(this);
+            NotesPanel.add(OpenNoteButton);      
+            notesField.updateUI(); 
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-    
-        counter++;
-        System.out.println("Hello World: " + counter);
+        if (e.getActionCommand() == "Create") {
+            File NewNote = new File(saveDirectory, "Note_" + numberOfNotes + ".txt");
+
+            try {
+                NewNote.createNewFile();
+            } catch (Exception exception) {
+                System.err.println(exception.toString());
+                return;
+            }
+
+            new Note(NewNote);
+            numberOfNotes++;
+            AddNoteEntryButton(NewNote);
+        }
+
+        else {
+            File NoteToOpen = new File(saveDirectory, e.getActionCommand());
+            new Note(NoteToOpen);
+        }
     }
 }
