d54dbe4c0d9b3ccb4643c63cb50a24e63e40d20e
patchworkcoding <Rose@NEW-MACHINE-01.com>
2025-12-04 20:05:22 -0600

    We are calling it feature complete

--- app/src/main/java/cs/_final/project/Note.java
+++ app/src/main/java/cs/_final/project/Note.java
@@ -7,13 +7,19 @@ import java.util.Scanner;
 
 import javax.swing.*;
 
-public class Note extends WindowAdapter {
+public class Note extends WindowAdapter implements ActionListener {
     File saveFile;
+    JFrame frame;
     JTextArea inputField;
     String text = "";
+    boolean isWindowOpen = false;
     
-    public Note(File _data) {
-        saveFile = _data;
+
+    public Note(File file) {
+        //Initializes the saveFile
+        saveFile = file;
+
+        //Creates a file reader
         Scanner Reader;
         try {
             Reader = new Scanner(saveFile);    
@@ -22,42 +28,88 @@ public class Note extends WindowAdapter {
             return;
         }
 
+        //Reads in the file data
         while (Reader.hasNextLine()) {
-            System.out.println(text);
             text += Reader.nextLine();
             text += System.lineSeparator();
-            
         }
 
-        //Create Window
-        JFrame frame = new JFrame(_data.getName());
-        frame.setSize(800, 600);
-        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        frame.addWindowListener(this);
-        //frame.addWindowStateListener(null);
-        frame.setVisible(true);
-
-        inputField = new JTextArea();
-        inputField.append(text);
-        frame.add(inputField);
-        
         Reader.close();
     }
 
-    @Override
-    public void windowClosing(WindowEvent e) {
+    public void openEditorWindow() {
+        //If the window isn't open Create it
+        if(!isWindowOpen) {
+            createEditorWindow();
+        }
+
+        //If the window is open bring it to the front
+        else {
+
+            frame.toFront();
+            inputField.requestFocus();
+        }
+    }
+
+    void createEditorWindow() {
+        //Creates Window
+        frame = new JFrame(saveFile.getName());
+        frame.setSize(800, 600);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.addWindowListener(this);
+        frame.setVisible(true);
+
+        //Creates organizing container
+        JPanel container = new JPanel(new BorderLayout());
+        frame.add(container);
+
+        //Creates text input field
+        inputField = new JTextArea();
+        inputField.append(text);
+        inputField.setLineWrap(true);
+        container.add(inputField, BorderLayout.CENTER);
         
-        
+        //Creates Button to save text to a file
+        JButton saveFileButton = new JButton("Save File");
+        saveFileButton.setActionCommand("save");
+        saveFileButton.addActionListener(this);
+        container.add(saveFileButton, BorderLayout.PAGE_END);
+
+        isWindowOpen = true;
+    }
+
+    public String getFileName() {
+        return saveFile.getName();
+    }
+
+    void saveTextToFile() {
+        //Saves off edited text into a file
         try {
             text = inputField.getText();
             FileWriter writer = new FileWriter(saveFile.getAbsolutePath());
             writer.write(text);
             writer.close();
+
         } catch (Exception exception) {
             System.err.println(exception.toString());
         }
-        
+    }
 
+    @Override
+    public void windowClosing(WindowEvent e) {
+        isWindowOpen = false;
+        saveTextToFile();
         super.windowClosing(e);
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand() == "save") {
+            saveTextToFile();
+        }
+
+        else if (e.getActionCommand() == "open") {
+            openEditorWindow();
+        }
     }
 }
--- app/src/main/java/cs/_final/project/NoteManager.java
+++ app/src/main/java/cs/_final/project/NoteManager.java
@@ -3,15 +3,15 @@ package cs._final.project;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.File;
+
 import javax.swing.*;
 
 public class NoteManager implements ActionListener {
     int numberOfNotes = 0;
     String saveDirectory = "Note_Folder";
+    
     JPanel notesField;
     JPanel NotesPanel;
-
-    
 
     public NoteManager() {
         CreateWindow();
@@ -25,13 +25,13 @@ public class NoteManager implements ActionListener {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
 
+        //Create a contain to hold all the "Open Note_##" buttons
         notesField = new JPanel(new BorderLayout());
         frame.add(notesField);
 
-
+        //Adds a button to create a new Note
         JButton CreateNewNoteButton = new JButton("Create Note");
-        CreateNewNoteButton.setActionCommand("Create");
-        CreateNewNoteButton.addActionListener(this);
+        CreateNewNoteButton.addActionListener(this); //Makes this the listener since it will handle note creations
         notesField.add(CreateNewNoteButton, BorderLayout.PAGE_END);
     }
 
@@ -39,52 +39,52 @@ public class NoteManager implements ActionListener {
         NotesPanel = new JPanel(new GridLayout(0,2, 10, 10));
         notesField.add(NotesPanel, BorderLayout.CENTER);
 
+        //Open up the save directory
         File NotesFolder = new File(saveDirectory);
+
+        //if it doesn't exist, create it
         if(!NotesFolder.exists())
             NotesFolder.mkdir();
 
         File[] noteFiles = NotesFolder.listFiles();
-        
-        
         if (noteFiles != null && noteFiles.length > 0) {
-            numberOfNotes = noteFiles.length;
 
-            for (File file : noteFiles) {
-                AddNoteEntryButton(file);
+            numberOfNotes = noteFiles.length; //Log the number of notes for later file naming
+
+            //Create a button for each note in the save directory
+            for (Integer i = 0; i < noteFiles.length; i++) {
+                AddNoteEntryButton(new Note(noteFiles[i]));
             }
 
         }
     }
 
-    void AddNoteEntryButton(File file) {
-            JButton OpenNoteButton = new JButton("open: " + file.getName());
+    void AddNoteEntryButton(Note noteToOpen) {
+            JButton OpenNoteButton = new JButton(noteToOpen.getFileName());
 
-            OpenNoteButton.setActionCommand(file.getName());
-            OpenNoteButton.addActionListener(this);
+            OpenNoteButton.setActionCommand("open");
+            OpenNoteButton.addActionListener(noteToOpen);
             NotesPanel.add(OpenNoteButton);      
             notesField.updateUI(); 
     }
 
+    //This action is only performed when the "Create New Note" button is pressed
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getActionCommand() == "Create") {
-            File NewNote = new File(saveDirectory, "Note_" + numberOfNotes + ".txt");
-
-            try {
-                NewNote.createNewFile();
-            } catch (Exception exception) {
-                System.err.println(exception.toString());
-                return;
-            }
-
-            new Note(NewNote);
-            numberOfNotes++;
-            AddNoteEntryButton(NewNote);
+        //Create the file to hold the Nth note
+        File newNoteFile = new File(saveDirectory, "Note_" + numberOfNotes + ".txt");
+        try {
+            newNoteFile.createNewFile();
+        } catch (Exception exception) {
+            System.err.println(exception.toString());
+            return;
         }
 
-        else {
-            File NoteToOpen = new File(saveDirectory, e.getActionCommand());
-            new Note(NoteToOpen);
-        }
+        //Instantiate the Nth note
+        Note createdNote = new Note(newNoteFile);
+        AddNoteEntryButton(createdNote);
+        createdNote.openEditorWindow();
+
+        numberOfNotes++;
     }
 }
